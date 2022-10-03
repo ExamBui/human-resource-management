dcc97dfa517f566269a8286a78e95426fde1bc5d
Thi Bui <thi.bui@mlpert.com>
2022-10-03 20:42:40 +0700

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="layout-content">
+        This is page Dashboard
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/SignUp.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import {
+  Layout,
+  Button,
+  Card,
+  Form,
+  Input,
+} from "antd";
+
+import { Link } from "react-router-dom";
+
+const { Content } = Layout;
+export default class SignUp extends Component {
+  render() {
+    const onFinish = (values) => {
+      console.log("Success:", values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
+    };
+    return (
+      <>
+        <div className="layout-default ant-layout layout-sign-up">
+          <Content className="p-0">
+
+            <Card
+              className="card-signup header-solid h-full ant-card pt-0"
+              title={<h5>Register With</h5>}
+              bordered="false"
+            >
+              <Form
+                name="basic"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                className="row-col"
+              >
+                <Form.Item
+                  name="Name"
+                  rules={[
+                    { required: true, message: "Please input your username!" },
+                  ]}
+                >
+                  <Input placeholder="Name" />
+                </Form.Item>
+                <Form.Item
+                  name="email"
+                  rules={[
+                    { required: true, message: "Please input your email!" },
+                  ]}
+                >
+                  <Input placeholder="Email" />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    { required: true, message: "Please input your password!" },
+                  ]}
+                >
+                  <Input placeholder="Password" />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    style={{ width: "100%" }}
+                    type="primary"
+                    htmlType="submit"
+                  >
+                    SIGN UP
+                  </Button>
+                </Form.Item>
+              </Form>
+              <p className="font-semibold text-muted text-center">
+                Already have an account?{" "}
+                <Link to="/sign-in" className="font-bold text-dark">
+                  Sign In
+                </Link>
+              </p>
+            </Card>
+          </Content>
+        </div>
+      </>
+    );
+  }
+}
